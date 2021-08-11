28e501ec056f4c8b71c0fd702d8a88b6f7a738fd
david <david.holder@gmail.com>
2021-08-11 22:32:06 +0100

    Update

--- cmd/root.go
+++ cmd/root.go
@@ -1,29 +1,37 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 	"os"
-
-	"github.com/spf13/viper"
+	"path"
+	"sort"
+	"strings"
+	"sync"
 )
 
-var cfgFile string
+type identifiedWorkload struct {
+	containerName, namespace, image, pod string
+}
+
+// byNamespace implements sort.Interface based on the namespace field.
+type byNamespace []identifiedWorkload
+
+func (n byNamespace) Len() int           { return len(n) }
+func (n byNamespace) Less(i, j int) bool { return n[i].namespace < n[j].namespace }
+func (n byNamespace) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
+
+// Channel buffer size
+const (
+	defaultKubeconfig = "~/.kube/config"
+	burst             = 50
+	qps               = 25
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -37,7 +45,197 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		//	cmd.Flags().Set("namespace", "default")
+		namespaceFromFlag, err := cmd.Flags().GetString("namespace")
+		kubeconfigFromFlag, err := cmd.Flags().GetString("kubeconfig")
+
+		if kubeconfigFromFlag == "" {
+			kubeconfigFromFlag, err = homeDir(defaultKubeconfig)
+			if err != nil {
+				handleError(err)
+			}
+		} else {
+			kubeconfigFromFlag, err = homeDir(kubeconfigFromFlag)
+			if err != nil {
+				handleError(err)
+			}
+		}
+
+		config, err := clientcmd.BuildConfigFromFlags("", kubeconfigFromFlag)
+
+		if err != nil {
+			handleError(err)
+		}
+
+		// Increase the Burst and QOS values
+		config.Burst = burst
+		config.QPS = qps
+
+		// Build client from  config
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			handleError(err)
+		}
+
+		ctx := context.Background()
+
+		// Get the list of containers
+		numberOfContainers, err := getTotalNumberOfContainers(ctx, clientset)
+		if err != nil {
+			handleError(err)
+		}
+
+		// Add a buffer of 10% - in case extra containers are spun whilst this app finishes
+		numberOfContainers += (numberOfContainers / 10)
+
+		// Grab the list of namespaces in the current context
+
+		var listOfNamespaces []string
+
+		if namespaceFromFlag != "" {
+			fmt.Printf("--namespace flag used, only scanning %s", namespaceFromFlag)
+			listOfNamespaces = append(listOfNamespaces, namespaceFromFlag)
+			fmt.Println(listOfNamespaces)
+		} else {
+			fmt.Printf("--namespace flag not used, only scanning all namespces\n")
+			listOfNamespaces, err = getNamespaces(ctx, clientset)
+			if err != nil {
+				handleError(err)
+			}
+		}
+
+		// Put all threads in a waitgroup so the channel can be closed once all threads have finished
+		var wg sync.WaitGroup
+		// Match the number of waitgroups to the number of namespaces.
+		// as each call to getPodsPerNamespace() will be in its own goroutine
+		wg.Add(len(listOfNamespaces))
+
+		// Retrieve responses from threaded calls
+		messages := make(chan identifiedWorkload, numberOfContainers)
+
+		//Iterate through the namespaces
+		for _, namespace := range listOfNamespaces {
+			//For each namespace, inspect the pods that reside it within a dedicated goroutine
+			fmt.Printf("Searching namespace: %s\n", namespace)
+			go func(n string) {
+				if err := getPodsPerNamespace(ctx, n, clientset, messages); err != nil {
+					handleError(err)
+				}
+				defer wg.Done()
+			}(namespace)
+		}
+		// Wait for all goroutines to finish
+		wg.Wait()
+
+		// As this uses a buffered channel, we need to explicitly close
+		close(messages)
+
+		var listOfWorkloads []identifiedWorkload
+		for element := range messages {
+			listOfWorkloads = append(listOfWorkloads, element)
+		}
+
+		sort.Sort(byNamespace(listOfWorkloads))
+		displayWorkloads(listOfWorkloads)
+
+	},
+}
+
+func handleError(err error) {
+	panic(err.Error())
+}
+
+func homeDir(filename string) (string, error) {
+	// Replace homedir reference if required
+	if strings.Contains(filename, "~/") {
+		homedir, err := os.UserHomeDir()
+		if err != nil {
+			handleError(err)
+		}
+		filename = strings.Replace(filename, "~/", "", 1)
+		filename = path.Join(homedir, filename)
+	}
+
+	// Check we can read the file
+	if _, err := os.Stat(filename); err != nil {
+		handleError(err)
+		return "", err
+	}
+	return filename, nil
+}
+
+// Return the list of namespaces in the cluster
+func getNamespaces(ctx context.Context, c *kubernetes.Clientset) ([]string, error) {
+	var listOfNamespaces []string
+	namespaces, err := c.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		handleError(err)
+		return listOfNamespaces, err
+	}
+
+	for _, namespace := range namespaces.Items {
+		listOfNamespaces = append(listOfNamespaces, namespace.Name)
+	}
+	return listOfNamespaces, nil
+}
+
+// Iterate through Namespace -> Pod -> Container and identify container images
+// using either :latest or no tag
+func getPodsPerNamespace(ctx context.Context, namespace string, clientSet *kubernetes.Clientset, c chan identifiedWorkload) error {
+	pods, err := clientSet.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		handleError(err)
+	}
+
+	for _, pod := range pods.Items {
+		for _, container := range pod.Spec.Containers {
+			if strings.Contains(container.Image, "latest") == true || strings.Contains(container.Image, ":") == false {
+				cont := identifiedWorkload{
+					containerName: container.Name,
+					namespace:     namespace,
+					image:         container.Image,
+					pod:           pod.Name,
+				}
+				c <- cont
+			}
+		}
+	}
+
+	return nil
+}
+
+//Display the results
+func displayWorkloads(w []identifiedWorkload) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Namespace", "Pod", "Container", "Image"})
+
+	for _, container := range w {
+		t.AppendRow(table.Row{
+			container.namespace, container.pod, container.containerName, container.image,
+		})
+		t.AppendSeparator()
+	}
+	t.SetStyle(table.StyleLight)
+
+	//render table
+	t.Render()
+}
+
+//Get the list of pods in the cluster. This will determine the buffer size of the channel
+func getTotalNumberOfContainers(ctx context.Context, clientSet *kubernetes.Clientset) (int, error) {
+
+	numberOfContainers := 0
+	pods, err := clientSet.CoreV1().Pods("").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return 0, err
+	}
+
+	for _, pod := range pods.Items {
+		numberOfContainers += len(pod.Spec.Containers)
+	}
+	return numberOfContainers, nil
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,39 +245,15 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra-demo-app.yaml)")
+	//	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra-demo-app.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".cobra-demo-app" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".cobra-demo-app")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
+	rootCmd.Flags().StringP("namespace", "n", "", "specify namespace")
+	rootCmd.Flags().StringP("kubeconfig", "k", "", "specify kubeconfig")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/David-VTUK/cobra-demo-app
 go 1.16
 
 require (
+	github.com/jedib0t/go-pretty/v6 v6.2.4
 	github.com/spf13/cobra v1.2.1
-	github.com/spf13/viper v1.8.1
+	k8s.io/apimachinery v0.22.0
+	k8s.io/client-go v0.22.0
 )
